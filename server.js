6e2a6390277f64f507f4a895168ad89c11c0fb76
Ivanets Polina <114954279+ivanetspolina@users.noreply.github.com>
2025-11-16 12:59:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.all('/', (req, res) => {
     .type('text/html; charset=utf-8')
     .send(`<!doctype html>
 <html>
-  <body style="font-family: Arial, sans-serif; text-align: center; padding-top: 40px;>
+  <body style="font-family: "Times New Roman", sans-serif; text-align: center; padding-top: 40px;>
     <h1 style="font-size: 36px; color: #2c3e50; font-weight: bold;>Hi, ${name}!</h1>
     <p style="font-size: 24px; color: #1C8139;">runtime: nodejs</p>
     <p style="font-size: 24px; color: #7E57C2;">region: ${region}</p>
